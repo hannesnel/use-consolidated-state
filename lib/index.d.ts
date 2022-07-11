e3c2a63522b03f1b12170def16faafa7b1b72ef7
Hannes Nel <hannes.nel@binderpos.com>
2022-07-11 14:38:30 +1200

    update gitignore and remove lib

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare type ConsolidatedState<T> = {
-    [P in keyof T as `set${Capitalize<string & P>}`]: (value: T[P]) => void;
-} & {
-    setState: (state: Partial<T>) => void;
-} & T;
-export default function useConsolidatedState<T>(initialState: T): ConsolidatedState<T>;
-export {};
-//# sourceMappingURL=index.d.ts.map
