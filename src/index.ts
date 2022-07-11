c7d223746ba118b4fa1164cadaf4a46211559c8b
Hannes Nel <hannes.nel@binderpos.com>
2022-07-11 14:32:00 +1200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+type ConsolidatedState<T> = {
+  [P in keyof T as `set${Capitalize<string & P>}`]: (value: T[P]) => void;
+} & {
+  setState: (state: Partial<T>) => void;
+} & T;
+
+export default function useConsolidatedState<T>(
+  initialState: T
+): ConsolidatedState<T> {
+  const [consolidatedState, setConsolidatedState] = useState<T>(initialState);
+
+  function functionCase(key: string) {
+    return "set" + key[0].toUpperCase() + key.slice(1);
+  }
+
+  const setters = Object.keys(consolidatedState).reduce<ConsolidatedState<T>>(
+    (acc, key) => {
+      return {
+        ...acc,
+        [functionCase(key)]: (value: any) =>
+          setConsolidatedState((prev) => ({
+            ...prev,
+            [key]: value,
+          })),
+      };
+    },
+    {} as ConsolidatedState<T>
+  );
+
+  const setState = (state: Partial<T>) =>
+    setConsolidatedState((prev) => ({
+      ...prev,
+      ...state,
+    }));
+
+  return {
+    ...consolidatedState,
+    ...setters,
+    setState,
+  };
+}
